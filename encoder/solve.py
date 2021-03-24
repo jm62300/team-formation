605c240425f2b91d5e0568545f623b89a50f71e2
lagniez <jmlagniez@gmail.com>
2021-03-24 11:38:21 +0100

    tf should be OK

--- encoder/solve.py
+++ encoder/solve.py
@@ -4,8 +4,6 @@ from parser import *
 
 from tf import *
 from ktf import *
-from ptf import *
-from rtf import *
 
 import sys
 import os
@@ -15,35 +13,28 @@ from math import ceil
 start_time = time.time()
 
 def help():
-    print("USAGE: ./solve.py FILE [-tf] [-ktf] [-ptf] [-rtf] [-k=1] [-t=90] [-cut] [-path=PATH]")
+    print("USAGE: ./solve.py FILE [-tf] [-ktf] [-k=1] [-b=1]")
     exit(1)
 
 if len(sys.argv) <= 1 or sys.argv[1] in ["-h", "-help"]:
     help()
 
 fileName = str(sys.argv[1])
-k, t, m, cut = 1, 90, "tf", False
+k, m, b = 1, "tf", 1
 
 # parse the options
 for o in sys.argv[2:]:
-    if o in ["-tf", "-ktf", "-ptf", "-rtf"]:
+    if o in ["-tf", "-ktf"]:
         m = o[1:]
     if o[:3] == "-k=":
         k = int(o.split('=')[1])
-    if o[:3] == "-t=":
-        t = int(o.split('=')[1])
-    if o == "-cut":
-        cut = True
-    if o[:6] == "-path=":
-        path = o.split('=')[1]
+    if o[:3] == "-b=":
+        b = int(o.split('=')[1])
 
 print("c Options:")
 print("c Method we run:", m)
 print("c The number of agents we can loose:", k)
-if (m == "ptf" or m == "rtf"):
-    print("c Cut used:", cut)
-if (m == "ptf"):
-    print("c Threshold concerning the quantity of skills we can lost:", t)
+print("c The bound:", b)
 
 print("c input file:", fileName)
 nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses = parse(fileName)
@@ -51,14 +42,11 @@ nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses = parse(fileName)
 print("c Information about the problem: ")
 print("c Number of agents:", nbAgents)
 print("c Number of skills:", nbSkills)
-print("c Number of clauses:", len(eclauses))
+print("c Number of integrity constraints:", len(eclauses))
 
 # apply the translation
 agentsProp, skillsProp = [], []
 if(m == "tf"):
-    solveTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses)
+    solveTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses, b)
 elif(m == "ktf"):
     solvekTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses, k)
-
-interval = time.time() - start_time
-print ('t', interval)
--- encoder/tf.py
+++ encoder/tf.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-from docplex.mp.model import Model
 from agent import *
+from pysat.pb import *
 
-
-def solveTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses):
+def solveTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses, bound):
     """
     Solve the TF problem given the input variables.
 
@@ -20,60 +18,35 @@ def solveTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses):
         for each id of skill we associate a structure that store data about it
     eclause : list
         each element containt an exclusion list
+    bound : int
+        the maximum value for the objective function
     """
-    model = Model()
-    agentsProp = [model.binary_var(str(i)) for i in range(nbAgents)]
+    agentsProp = [i + 1 for i in range(nbAgents)]
+    clauses = []
 
     # the set of constraints that ensure that each skill is supported by at least one agent
     for s in mapOfSkills:
-        model.add_constraint(model.sum(agentsProp[mapOfAgents[a].id] for a in mapOfSkills[s].l) >= 1)
-
-    # integrity constraints
-    for cl in eclauses:
-        model.add_constraint(model.sum(agentsProp[mapOfAgents[cl[i]].id] for i in range(len(cl))) <= 1)
-
-    # the function we search to minimize
-    cost = model.sum(agentsProp[mapOfAgents[a].id] * mapOfAgents[a].w1 for a in mapOfAgents)
-
-    model.minimize(cost)
-    solution = model.solve()
-
-    if solution != None:
-        print("v", end=" ")
-        for a in agentsProp:
-            if(solution[a] == 1):
-                print(a, end=" ")
-        print()
-        print("o", int(model.objective_value))
-    else:
-        print("No folution found")
-
-
-def toTF(nbAgents, nbSkills, mapOfAgents, mapOfSkills, eclauses, outputFile):
-    # create boolean variable indexes
-    agentsProp = [i for i in range(1, nbAgents + 1)]
-    idxFresh = agentsProp[-1] + 1
-
-    print("c map for agents (in the order): ", str(agentsProp))
-
-    # constraint we want to optimize
-    print("min: ", end="", file=outputFile)
-    for a in mapOfAgents:
-        agent = mapOfAgents[a]
-        print("+" + str(agent.w1) + "*x" + str(agentsProp[agent.id]), end=" ", file=outputFile)
-    print(";", file=outputFile)
-
-    for s in mapOfSkills:
-        for a in mapOfSkills[s].l:
-            print("+1*x" + str(agentsProp[mapOfAgents[a].id]), end=" ", file=outputFile)
-        print(" >=", 1, ";", file=outputFile)
+        clauses.append([agentsProp[mapOfAgents[a].id] for a in mapOfSkills[s].l])
 
     # integrity constraints
     for cl in eclauses:
         for i in range(len(cl)):
-            assert cl[i] in mapOfAgents
-            print("+1*x" + str(agentsProp[mapOfAgents[cl[i]].id]), end=" ", file=outputFile)
-        print(" <= 1 ;", file=outputFile)
+            for j in range(i+1, len(cl)):
+                clauses.append([-agentsProp[mapOfAgents[cl[i]].id], -agentsProp[mapOfAgents[cl[j]].id]])
 
-    outputFile.flush()
-    return agentsProp
+    # the function we search to minimize
+    agents = [agentsProp[mapOfAgents[a].id] for a in mapOfAgents]
+    weights = [mapOfAgents[a].w1 for a in mapOfAgents]
+
+    print(agents)
+    print(weights)
+
+    cnf = PBEnc.leq(lits=agents, weights=weights, bound=bound, encoding=EncType.bdd)
+    clauses += cnf.clauses    
+
+    # print out following the Dimacs format.
+    print("p cnf", nbAgents, len(clauses))
+    for cl in clauses:
+        for l in cl:
+            print(l, end=" ")
+        print("0")
