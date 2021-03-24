32b1a0f1190aa68704767c27a9047633c07dc2f0
lagniez <jmlagniez@gmail.com>
2021-03-24 11:56:05 +0100

    clean

--- encoder/agent.py
+++ encoder/agent.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/usr/bin/python3
-
 class Agent:
     name = ""
     id = -1
--- encoder/solve.py
+++ encoder/solve.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Copyright (C) 2021  Lagniez
 #
 # This program is free software: you can redistribute it and/or modify
@@ -13,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/usr/bin/python3
 from agent import *
 from parser import *
 
@@ -22,10 +22,6 @@ from ktf import *
 
 import sys
 import os
-import time
-from math import ceil
-
-start_time = time.time()
 
 def help():
     print("USAGE: ./solve.py FILE [-tf] [-ktf] [-k=1] [-b=1]")
